2d2d78c611aec17b53f769da7725db3567d45e70
kaseris <mich.kaseris@gmail.com>
2022-07-15 15:51:50 +0300

    now copies data too to install dir

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import os
+import os.path as osp
 from setuptools import setup
+
+
+def data_package_files(directory):
+    paths = []
+    for path, directories, filenames in os.walk(directory):
+        for filename in filenames:
+            paths.append(('mannequin/'+path, [osp.join(path, filename)]))
+    return paths
+
+
+extra_files = data_package_files('data')
 
 setup(name='mannequin',
       version='0.1',
@@ -12,4 +25,8 @@ setup(name='mannequin',
                 'mannequin/lerp',
                 'mannequin/detection',
                 'mannequin/retrieval3d',
-                'mannequin/TPS'])
+                'mannequin/TPS'],
+      package_data={'': ['conf.json']},
+      data_files=extra_files,
+      include_package_data=True
+      )
