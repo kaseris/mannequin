f3ca98d0a0d95cecc2edb9b167303d7e2482199e
kaseris <mich.kaseris@gmail.com>
2022-09-01 11:14:42 +0300

    3D retrieval standalone

--- tools/retrieve_3d.py
+++ tools/retrieve_3d.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import os.path as osp
+import re
+import shutil
 
 import mannequin as mnq
 
@@ -27,12 +29,42 @@ def get_config():
     return parser.parse_args()
 
 
+def save_image_result(query_name, retrieved_paths):
+    """Saves the 3D pre-rendered images of the retrieved objects."""
+    current_dir = os.getcwd()  # Find the directory where the function is called
+    # /path/to/dataset/[category]/[object_name]/[object_name.obj] -> object_name
+    dirname = osp.dirname(query_name)
+    object_name = dirname.split('/')[-1]
+
+    _path_to_save = osp.join(current_dir, object_name)
+    if not osp.exists(_path_to_save):
+        os.mkdir(_path_to_save)
+
+    # Save the query image in the designated path
+    for fname in os.listdir(dirname):
+        if 'camera_front' in fname:
+            _img_name = fname
+
+    shutil.copy(osp.join(dirname, _img_name), _path_to_save)
+
+    for retrieved_path in retrieved_paths:
+        _directory = osp.dirname(retrieved_path)
+
+        for _fname in os.listdir(_directory):
+            if 'camera_front' in _fname:
+                copy_this = _fname
+        shutil.copy(osp.join(_directory, copy_this), _path_to_save)
+
+
 if __name__ == '__main__':
     config = get_config()
-
+    print('Start')
+    path_to_data = '/home/kaseris/Documents/3DGarments'
     retrieved = infer(input_filename=config.in_file,
                       cluster_centers_filename=config.clusters_filename,
                       global_descriptors_filename=config.global_descriptors_file,
                       n_retrieved=config.n_retrieved,
                       voxel_size=config.voxel_size,
+                      data_path=path_to_data,
                       hist_size=config.hist_size)
+    save_image_result(config.in_file, retrieved)
