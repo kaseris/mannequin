c83be6185df634e2aa2b11a4e7fccd2db388e7ae
kaseris <mich.kaseris@gmail.com>
2022-11-17 15:09:00 +0200

    Taken care of the case of clockwise orientation of a pattern.

--- tools/individual_pattern.py
+++ tools/individual_pattern.py
@@ -4,7 +4,9 @@ import os.path as osp
 
 import numpy as np
 
+from mannequin.detection import detect_keypoints
 from mannequin.fileio import read_coords_from_txt
+from mannequin.primitives.utils import sort_xy
 
 
 class IndividualPattern:
@@ -26,33 +28,38 @@ class IndividualPattern:
             if not 'individual patterns' in children:
                 raise FileNotFoundError(f'`individual patterns` directory was not found in this folder.')
 
-            ind_patterns = osp.join(self.garment_dir, 'individual patterns')
+            ind_patterns = osp.join(self.garment_dir, 'individual pattcd erns')
             for f in IndividualPattern.pattern_files:
                 if f in os.listdir(ind_patterns):
-                    self.__patterns[f.replace('.xyz', '')] = read_coords_from_txt(osp.join(ind_patterns, f),
-                                                                                  delimiter=',')
+                    self.__patterns[f.replace('.xyz', '')] = self.__rearrange(
+                        read_coords_from_txt(osp.join(ind_patterns, f),
+                                             delimiter=','))
 
     def replace(self, curve, region):
         def find_nearest(array, value):
             dist = array - value
             norm = np.linalg.norm(dist, axis=1)
             return norm.argmin()
-
         coords_region = self[region]
         reverse = False
         # Find if CW/CCW
         curve_start = curve[0, :]
         curve_end = curve[-1, :]
-        idx_start = find_nearest(coords_region, curve_start)
-        idx_end = find_nearest(coords_region, curve_end)
-        if idx_end < idx_start:
+        idx_start_ = find_nearest(coords_region, curve_start)
+        idx_end_ = find_nearest(coords_region, curve_end)
+        if idx_end_ < idx_start_:
             reverse = True
 
-        if reverse:
-            curve = curve[::-1]
         region_copy = copy.deepcopy(self[region])
-        region_copy_part1 = region_copy[:idx_start]
-        region_copy_part2 = region_copy[idx_end+1:]
+        if reverse:
+            idx_start = region_copy.shape[0] - idx_end_ - 1
+            idx_end = region_copy.shape[0] - idx_start_ - 1
+            region_copy = region_copy[::-1]
+            region_copy_part1 = region_copy[:idx_end]
+            region_copy_part2 = region_copy[idx_start:]
+        else:
+            region_copy_part1 = region_copy[:idx_start_]
+            region_copy_part2 = region_copy[idx_end_+1:]
         region_new = np.vstack((region_copy_part1, curve, region_copy_part2))
         self.replace_region(region, region_new)
 
@@ -62,6 +69,22 @@ class IndividualPattern:
 
     def replace_region(self, region, curve):
         self.__patterns[region] = curve
+
+    def __rearrange(self, array):
+        key_points = detect_keypoints(array)
+        sorted_keypoints = sort_xy(key_points)
+        idx_sorted_by_y_front = np.argsort(sorted_keypoints[:, 1])[:2]
+        lowest_points_front = sorted_keypoints[idx_sorted_by_y_front]
+        where_max_X_front = np.argmax(lowest_points_front[:, 0])
+
+        # Find the index of the lower-right point in the keypoints array
+        # We store it in the idx_key_point_start var
+        coords_key_point_start_front = lowest_points_front[where_max_X_front]
+        idx_key_point_start_front = np.where(np.all(sorted_keypoints == coords_key_point_start_front,
+                                                    axis=1))[0][0]
+        idx = int(np.where(np.all(sorted_keypoints[idx_key_point_start_front] == array, axis=1))[0][0])
+        part1 = array[:idx + 1]
+        return np.vstack((array[idx:], part1))
 
     def __getitem__(self, item):
         if item not in [l.replace('.xyz', '') for l in IndividualPattern.pattern_files]:
