aaac5295f7b06de4737a86321d5bccfde4224fdd
kaseris <mich.kaseris@gmail.com>
2022-11-24 14:36:48 +0200

    A model interface that holds a pattern's data along with its interactive lines. Works plug-n-play with the layout's interactive preview.

---
+++ tools/app_models.py
@@ -0,0 +1,61 @@
+import uuid
+
+from os import PathLike
+from typing import Union
+
+from individual_pattern import IndividualPattern
+from interactive_mpl import InteractiveLine
+
+
+class IndividualPatternModel:
+
+    def __init__(self):
+        self.__ind_pat = None
+        self.__interactive_lines = []
+
+    def build(self, garment_dir: Union[str, PathLike]):
+        self.__ind_pat = IndividualPattern(garment_dir=garment_dir)
+        self.__build_interactive_lines()
+
+    def clear(self):
+        self.__ind_pat = None
+        self.__interactive_lines = None
+        self.notify_controller(event_type='data_cleared')
+
+    def __build_interactive_lines(self):
+        # TODO: I could use the __ind_pat.pattern keys to label the interactive lines
+        # It would be a convenient way to label my regions on hover
+        for region in self.__ind_pat.patterns.keys():
+            points = self.__ind_pat[region]
+            uid = str(uuid.uuid4())
+            line = InteractiveLine([points], id=uid)
+            self.__interactive_lines.append(line)
+
+    def update(self, new_garment_dir):
+        self.clear()
+        self.__ind_pat = IndividualPattern(new_garment_dir)
+        self.__build_interactive_lines()
+        self.notify_controller(event_type='data_updated')
+
+    @property
+    def ind_pat(self):
+        return self.__ind_pat
+
+    @property
+    def interactive_lines(self):
+        return self.__interactive_lines
+
+    def notify_controller(self,
+                          controller=None,
+                          event_type=None):
+        r"""
+        Notifies the controller that an event happened. For example if the user requests for the data to be cleared,
+        the method will let the controller know that the model data is now empty and issue a command to its bound view
+        to clear the drawn data. Same applies for the change of data.
+
+        Args:
+            controller (Any): A controller that binds a IndividualPatternModel instance to a view.
+            event_type (str): An event that lets the controller take a specific action to the corresponding view.
+                Types can be: `clear`, `data_updated`.
+        """
+        pass
