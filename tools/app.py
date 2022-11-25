6c8a7767b452ad4b520580a95a19fd89e697f498
kaseris <mich.kaseris@gmail.com>
2022-11-25 10:03:17 +0200

    Entry poitn test

--- tools/app.py
+++ tools/app.py
@@ -1,16 +1,24 @@
 from app_models import IndividualPatternModel
+from controllers import ControllerPatternModelPreview
 from layout import UI
 
 
 class App:
     def __init__(self):
-        self.ui = UI(test_shown=False)
+        self.ui = UI(test_shown=True)
         self.pat_model = IndividualPatternModel()
-        # self.pat_model.build('/home/kaseris/Documents/database/dress/d1/W62K92')
-        # self.ui.layout.frame_pattern_preview.draw_pattern(self.pat_model.interactive_lines)
 
     def run(self):
+        self.setup()
         self.ui.run()
+
+    def setup(self):
+        self.pat_model.build('/home/kaseris/Documents/database/dress/d1/W62K92')
+        self.ui.layout.frame_pattern_preview.draw_pattern(self.pat_model.interactive_lines)
+        self.controller_pat_preview = ControllerPatternModelPreview()
+        self.controller_pat_preview.couple(self.pat_model, self.ui.layout.frame_pattern_preview)
+        self.controller_pat_preview.bind_('pick_event', self.controller_pat_preview.on_pick)
+        self.controller_pat_preview.bind_('key_press_event', self.controller_pat_preview.on_key_press)
 
 
 if __name__ == '__main__':
