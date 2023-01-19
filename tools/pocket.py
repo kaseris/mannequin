194a1e2a6cf83fac6a9eda3157a19c5f7907de92
kaseris <mich.kaseris@gmail.com>
2023-01-19 16:36:43 +0200

    Pocket data structure model

---
+++ tools/pocket.py
@@ -0,0 +1,20 @@
+class Pocket:
+
+    def __init__(self):
+        self.__interactive_line = None
+
+    def edit(self):
+        pass
+
+    def build(self):
+        pass
+
+    def update(self):
+        pass
+
+    def remove(self):
+        pass
+
+    @property
+    def interactive_line(self):
+        return self.__interactive_line
