86c0905a7c32b82a25f1c44d5a5b219a5c7a0120
kaseris <mich.kaseris@gmail.com>
2023-01-23 13:26:10 +0200

    import

--- mannequin/fileio/__init__.py
+++ mannequin/fileio/__init__.py
@@ -1,3 +1,4 @@
-from .fileio import read_coords_from_txt, read_seam_structure, save_txt, read_obj, check_obj
+from .fileio import read_coords_from_txt, read_seam_structure, save_txt, read_obj, check_obj, write_array_as_ind_pat
 
-__all__ = ['read_coords_from_txt', 'read_seam_structure', 'save_txt', 'read_obj', 'check_obj']
+__all__ = ['read_coords_from_txt', 'read_seam_structure', 'save_txt', 'read_obj', 'check_obj',
+           'write_array_as_ind_pat']
